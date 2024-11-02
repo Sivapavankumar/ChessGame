e100bbad573e18cdaca597b4c4a0e381ef004600
Sivapavankumar <156369837+Sivapavankumar@users.noreply.github.com>
2024-11-01 23:43:54 -0500

    added Algebraic notation, Board, Board Simulator

--- gradlew.bat
+++ gradlew.bat
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-=======
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-
->>>>>>> 7ec7af5525de2e462ce7036ddcc9c9bc7a03a231
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -32,27 +13,15 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-<<<<<<< HEAD
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
-=======
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
-
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
->>>>>>> 7ec7af5525de2e462ce7036ddcc9c9bc7a03a231
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-<<<<<<< HEAD
 if "%ERRORLEVEL%" == "0" goto init
-=======
-if "%ERRORLEVEL%" == "0" goto execute
->>>>>>> 7ec7af5525de2e462ce7036ddcc9c9bc7a03a231
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
@@ -66,11 +35,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-<<<<<<< HEAD
 if exist "%JAVA_EXE%" goto init
-=======
-if exist "%JAVA_EXE%" goto execute
->>>>>>> 7ec7af5525de2e462ce7036ddcc9c9bc7a03a231
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
@@ -80,7 +45,6 @@ echo location of your Java installation.
 
 goto fail
 
-<<<<<<< HEAD
 :init
 @rem Get command-line arguments, handling Windows variants
 
@@ -96,21 +60,13 @@ if "x%~1" == "x" goto execute
 
 set CMD_LINE_ARGS=%*
 
-=======
->>>>>>> 7ec7af5525de2e462ce7036ddcc9c9bc7a03a231
 :execute
 @rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-<<<<<<< HEAD
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-=======
-
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
->>>>>>> 7ec7af5525de2e462ce7036ddcc9c9bc7a03a231
 
 :end
 @rem End local scope for the variables with windows NT shell
